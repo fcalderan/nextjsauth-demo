152ed60cdd479f8380f86592885d94d8086eeab9
Fabrizio Calderan <fabrizio.calderan@akqa.com>
2023-01-23 14:08:39 +0100

    test performance

--- components/layout/layout.js
+++ components/layout/layout.js
@@ -1,15 +1,14 @@
+//import dynamic from "next/dynamic";
 import MainNavigation from "./main-navigation";
 
-import dynamic from "next/dynamic";
-
-const DynMainNavigation = dynamic(() => import("./main-navigation"), {
-  loading: () => "Loading...",
-});
+// const DynMainNavigation = dynamic(() => import("./main-navigation"), {
+//   loading: () => "Loading...",
+// });
 
 function Layout(props) {
   return (
     <>
-      <DynMainNavigation />
+      <MainNavigation />
       <main>{props.children}</main>
     </>
   );
