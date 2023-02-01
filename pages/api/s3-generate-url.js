76e5e499788d2a0869101f7d4fd4de5831b8e55a
Fabrizio Calderan <fabrizio.calderan@akqa.com>
2023-02-01 10:08:57 +0100

    Force deployment

--- pages/api/s3-generate-url.js
+++ pages/api/s3-generate-url.js
@@ -3,6 +3,5 @@ import { generateTemporaryUrl } from "next-s3-upload";
 export default async function GenerateTemporaryUrlHandler(req, res) {
   const key = req.query.key;
   const S3TempUrl = await generateTemporaryUrl(key);
-
   res.status(200).json({ S3TempUrl });
 }
